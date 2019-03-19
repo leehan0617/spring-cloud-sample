d0e8dcc302aa1bc32db551ad79ad2b2a9998403b
heron.lee <heron.lee@kakaocorp.com>
2019-03-19 14:20:57 +0900

    쿠폰 API, 프로필 API 서버 eureka서버에 등록

--- profile-api/src/main/java/com/profile/api/ProfileApplication.java
+++ profile-api/src/main/java/com/profile/api/ProfileApplication.java
@@ -2,16 +2,13 @@ package com.profile.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@RibbonClients({
-        @RibbonClient(name = "coupon-api")
-               })
+@EnableDiscoveryClient
 public class ProfileApplication {
 
     @LoadBalanced
