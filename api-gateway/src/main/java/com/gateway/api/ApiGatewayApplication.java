56e025ce9171d8c15f5afac157584dbbc3d7d0d2
heron.lee <heron.lee@kakaocorp.com>
2019-03-19 16:37:47 +0900

    zuul, eureka 연동

--- api-gateway/src/main/java/com/gateway/api/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/gateway/api/ApiGatewayApplication.java
@@ -2,6 +2,7 @@ package com.gateway.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  */
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
