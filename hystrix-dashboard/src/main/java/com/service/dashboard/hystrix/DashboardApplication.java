68cb74e646f890636d7ac7a6cca272213cc12ffb
heron.lee <heron.lee@kakaocorp.com>
2019-03-20 16:26:08 +0900

    hystrix dashboard 적용 (profile api에만 hystrix command가있어서 profile api만 모니터링 가능)

---
+++ hystrix-dashboard/src/main/java/com/service/dashboard/hystrix/DashboardApplication.java
@@ -0,0 +1,16 @@
+package com.service.dashboard.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * Created by heron.lee 2019-03-20
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class DashboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardApplication.class, args);
+    }
+}
